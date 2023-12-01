06c73e5a6dac85aa45da030f8dde75633dac2653
bcoover <bcoover@costco.com>
2023-12-01 14:45:39 -0500

    updated elasped time display

--- file_tools.py
+++ file_tools.py
@@ -19,11 +19,16 @@ def format_elapsed_time(elapsed_seconds:float) -> str:
 
     if elapsed_seconds > 60:
         minutes, elapsed_seconds = divmod(elapsed_seconds, 60)
-
     hours = round(hours)
     minutes = round(minutes)
     seconds = round(elapsed_seconds, 2)
-
+    if hours < 10:
+        hours = f"0{hours}"
+    if minutes < 10:
+        minutes = f"0{minutes}"
+    if seconds < 10:
+        seconds = f"0{seconds}"
+        
     return f'{hours}:{minutes}:{seconds}'
 
 def get_timestamp_for_filename(time_obj: time) -> str:
