520aeb6e894bfa46da6fb1710324ca99a5c17931
bcoover <bcoover@costco.com>
2023-12-01 15:41:17 -0500

    fix

--- log_tools.py
+++ log_tools.py
@@ -66,7 +66,7 @@ def setup_logger(filename='') -> logging.Logger:
     'formatters': { 
         'standard': { 
             'format': '%(asctime)s.%(msecs)03d [%(levelname)s] %(name)s: %(message)s',
-            'datefmt': "%Y-%m-%d %H:%M:%S %Z%z"
+            'datefmt': "%Y-%m-%d %Z%z %H:%M:%S"
         },
     },
     'handlers': { 
